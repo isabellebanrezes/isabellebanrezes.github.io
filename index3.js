baacfcf1936a29c6e459efc38d70a71e320ba7bc
isabelle BANREZES <isabanrezes@gmail.com>
2024-01-23 21:29:42 +0100

    ajout js

--- index3.js
+++ index3.js
@@ -6,8 +6,33 @@ titreExperience.addEventListener("click", () => {
   console.log("j'ai cliqué sur le titre exprérience !");
 });
 
-const titreClicked = document.querySelector(".titre-emploi");
+const emplois = document.querySelector(".titre-emploi");
 
-titreClicked.addEventListener("click", () => {
-  titreClicked.classList.add("titre-emploi-clicked");
+emplois.addEventListener("click", () => {
+  emplois.classList.toggle("titre-emploi-clicked");
+});
+
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  mousemove.style.background = "blue";
+});
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.background = "grey";
+});
+
+const boxEmploi = document.querySelector(".titre-emploi");
+
+boxEmploi.addEventListener("mouseenter", () => {
+  boxEmploi.style.background = "blue";
+});
+
+boxEmploi.addEventListener("mouseout", () => {
+  boxEmploi.style.background = "grey";
 });
