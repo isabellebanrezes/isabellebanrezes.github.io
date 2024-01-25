c885fb2a90848707855d56b3bccfe5cd95c96d6f
isabelle BANREZES <isabanrezes@gmail.com>
2024-01-25 17:50:52 +0100

    modification curseur

--- index3.js
+++ index3.js
@@ -20,36 +20,29 @@ window.addEventListener("mousemove", (e) => {
 });
 
 window.addEventListener("mousedown", () => {
-  mousemove.style.background = "blue";
+  mousemove.style.background = "rgba(34, 108, 212, 0.889)";
 });
 
 window.addEventListener("mouseup", () => {
-  mousemove.style.background = "grey";
+  mousemove.style.background = "rgba(194,196, 198)";
 });
 
 const titleFormation = document.querySelector(".titre-formation");
 
 titleFormation.addEventListener("mouseenter", () => {
-  titleFormation.style.color = "green";
+  titleFormation.style.color = "rgba(3,93,151)";
 });
 
 titleFormation.addEventListener("mouseout", () => {
-  titleFormation.style.color = "black";
+  titleFormation.style.color = "rgba(17,112,175";
 });
 
 const titleExperience = document.querySelector(".titre-experience");
 
 titleExperience.addEventListener("mouseover", () => {
-  titleExperience.style.color = "red";
+  titleExperience.style.color = "rgba(153,37,17)";
 });
 
 titleExperience.addEventListener("mouseout", () => {
   titleExperience.style.color = "black";
-});
-
-const cursor = document.querySelector(".cursor");
-
-window.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.pageX + "px";
-  cursor.style.top = e.pageY + "px";
 });
